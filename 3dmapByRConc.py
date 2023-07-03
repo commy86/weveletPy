fa088ba311b7b49255a2e2e1eb019757558ae57e
Azufre451 <moreno.zolfo@gmail.com>
2023-07-04 01:56:52 +0900

    changing concurrent to multiprocessing imap unordered

--- 3dmapByRConc.py
+++ 3dmapByRConc.py
@@ -3,6 +3,10 @@ from scipy.signal import chirp
 import matplotlib.pyplot as plt
 import pywt
 import concurrent.futures
+import multiprocessing as mp
+import sys,os
+
+
 
 def process_f(f, x, XF, M, t, params):
     r, m, p, k = params
@@ -27,10 +31,17 @@ def Proposed_ST(x, fe, fmax, params):
     Pas = int(fmax * M / fe)
     
     # Use concurrent.futures to parallelize the loop
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        S_matrix_futures = [executor.submit(process_f, f, x, XF, M, t, params) for f in range(1, Pas + 1)]
+    #with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+    #    S_matrix_futures = [executor.submit(process_f, f, x, XF, M, t, params) for f in range(1, Pas + 1)]
+
+    nResult=[]
+    for f in range(1, Pas + 1):
+        #print(f,x,XF,M,t)
+        nResult.append( process_f(f, x, XF, M, t, params))
     
-    S_matrix = np.vstack([future.result() for future in S_matrix_futures])  # Convert the list of arrays to a 2D array
+    #S_matrix = np.vstack([future.result() for future in S_matrix_futures])  # Convert the list of arrays to a 2D array
+    S_matrix = np.vstack(nResult)  # Convert the list of arrays to a 2D array
+
     N_S_matrix = S_matrix / np.sqrt(np.sum(np.sum(S_matrix * np.conj(S_matrix))))
     CM_proposed = 1 / np.sum(np.sum(np.abs(N_S_matrix)))
     
@@ -38,7 +49,13 @@ def Proposed_ST(x, fe, fmax, params):
     
     return y, CM_proposed
 
-def evaluate_params(sig, fe, fmax, params):
+#def evaluate_params(sig, fe, fmax, params):
+def evaluate_params(paramList):
+
+    sig, fe, fmax, params = paramList
+
+    #print("Worker {}".format(params))
+
     r = params[0]
     m = params[1]
     p = params[2]
@@ -47,6 +64,8 @@ def evaluate_params(sig, fe, fmax, params):
     params = [r, m, p, k]
     result, CM_proposed = Proposed_ST(sig, fe, fmax, params)
     score = CM_proposed
+
+    print("Worker {} reporting score {} with params {}".format(os.getpid(),score,params))
 
     return score, params
 
@@ -62,7 +81,17 @@ if __name__ == '__main__':
     freqs = np.linspace(1, nq_f, 500)  # 周波数配列
 
     # シグナルの定義
-    sig = pywt.data.demo_signal('WernerSorrows', t.size)
+    #sig = pywt.data.demo_signal('WernerSorrows', t.size)
+
+    sig = (np.exp(2*np.pi*1j*(N/2**2)*t) * ((0/8<=t) & (t<1/8)) +
+        np.exp(2*np.pi*1j*(N/2**3)*t) * ((1/8<=t) & (t<2/8)) +
+        np.exp(2*np.pi*1j*(N/2**4)*t) * ((2/8<=t) & (t<3/8)) +
+        np.exp(2*np.pi*1j*(N/2**5)*t) * ((3/8<=t) & (t<4/8)) +
+        np.exp(2*np.pi*1j*(N/2**6)*t) * ((4/8<=t) & (t<5/8)) +
+        np.exp(2*np.pi*1j*(-N/2**2)*t) * ((5/8<=t) & (t<6/8)) +
+        np.exp(2*np.pi*1j*(-N/2**3)*t) * ((6/8<=t) & (t<7/8)) +
+        np.exp(2*np.pi*1j*(-N/2**4)*t) * ((7/8<=t) & (t<8/8))).astype('float64')
+
     fe = fs  # サンプリング周波数
     fmax = nq_f  # 最大周波数
     param_range = {
@@ -76,28 +105,61 @@ if __name__ == '__main__':
     best_score = -np.inf
 
     results = []
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        for r in param_range['r']:
-            for m in param_range['m']:
-                for p in param_range['p']:
-                    for k in param_range['k']:
-                        params = [r, m, p, k]
-                        future = executor.submit(evaluate_params, sig, fe, fmax, params)
-                        results.append(future)
+
+
+    paramList=[]
+    print("Building Params!")
+    for r in param_range['r']:
+        for m in param_range['m']:
+            for p in param_range['p']:
+                for k in param_range['k']:
+                    paramList.append( (sig,fe,fmax,(r,m,p,k)) )
+
+
+    ##if you use spawn on mac, try to force to use fork
+    ##mp.set_start_method('fork')
+
+    print("Feeding a pool (strategy = {}) the pool with {} workers".format(mp.get_start_method(),len(paramList)))
+    with mp.Pool(processes=10) as pool:
+        results = [_ for _ in pool.imap_unordered(evaluate_params , paramList,chunksize=10)]
+        #for d in rpt:
+        #    results = rpt
+
+    ## with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
+    ##     for r in param_range['r']:
+    ##         for m in param_range['m']:
+    ##             for p in param_range['p']:
+    ##                 for k in param_range['k']:
+    ##                     params = [r, m, p, k]
+    ##                     print(params)
+    ##                     future = executor.submit(evaluate_params, sig, fe, fmax, params)
+    ##                     results.append(future)
 
     # CM_proposedの値とパラメータの組を保存するリスト
     cm_values = []
     params_list = []
 
-    # 最適なスコアとそのパラメータを見つける
-    for future in concurrent.futures.as_completed(results):
-        score, params = future.result()
+    #best_score = min([ score,_ for score,_ in results ])
+
+    for score,params in results:
         if score > best_score:
             best_score = score
             best_params = params
-        # CM_proposedの値とパラメータを保存
         cm_values.append(score)
         params_list.append(params)
+
+
+    print('Best Score: {}, Best Params: {}'.format(best_score,best_params))
+
+    # 最適なスコアとそのパラメータを見つける
+    ##for future in concurrent.futures.as_completed(results):
+    ##    score, params = future.result()
+    ##    if score > best_score:
+    ##        best_score = score
+    ##        best_params = params
+    ##    # CM_proposedの値とパラメータを保存
+    ##    cm_values.append(score)
+    ##    params_list.append(params)
 
     params5 = best_params
 
@@ -132,6 +194,7 @@ if __name__ == '__main__':
         cm_values_r = [cm_values[i] for i in range(len(cm_values)) if params_list[i][0] == r]
         plt.figure()
         plt.plot(cm_values_r)
+
         plt.xlabel('Iteration')
         plt.ylabel('CM_proposed')
 
